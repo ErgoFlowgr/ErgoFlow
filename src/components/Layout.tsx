41ed28291e52622c2726f441df2fd6e24818d324
Stavros <stavros@ergoflow.app>
2026-05-06 14:42:18 +0300

    auto-save 2026-05-06 14:42

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
-import { NavLink, useLocation } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ipc, isElectron } from '../lib/electron'
 import { getSettings } from '../lib/db'
 
